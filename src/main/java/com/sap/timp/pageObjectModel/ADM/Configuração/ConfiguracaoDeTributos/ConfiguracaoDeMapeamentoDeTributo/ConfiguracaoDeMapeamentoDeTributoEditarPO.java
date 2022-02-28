763724edff35a186ac04234d47524f4d0b90ebd1
steven Lezama <steven.le~zama@sinch.com>
2022-02-28 12:06:56 -0600

    configurations

--- src/main/java/com/sap/timp/pageObjectModel/ADM/Configuração/ConfiguracaoDeTributos/ConfiguracaoDeMapeamentoDeTributo/ConfiguracaoDeMapeamentoDeTributoEditarPO.java
+++
@@ -1,5 +0,0 @@
-package com.sap.timp.pageObjectModel.ADM.Configuração.ConfiguracaoDeTributos.ConfiguracaoDeMapeamentoDeTributo;
-
-public class ConfiguracaoDeMapeamentoDeTributoEditarPO {
-
-}
--- src/main/java/com/sap/timp/pageObjectModel/TCC/BancoDeOcorrencias/Executados/BancodeOcorrenciasExecutadosVisualizarPO.java
+++
@@ -1,7 +0,0 @@
-package com.sap.timp.pageObjectModel.TCC.BancoDeOcorrencias.Executados;
-
-import com.sap.timp.base.TestBaseKenssy;
-
-public class BancodeOcorrenciasExecutadosVisualizarPO extends TestBaseKenssy{
-
-}
